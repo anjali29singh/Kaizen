c6c6bfc0333a7575122f894f9223b52f5dcf2712
anjali29singh <anjalisingh930260@gmail.com>
2024-02-17 12:54:36 +0530

    .

---
+++ myapp/src/app/components/todoComp/todoComp.tsx
@@ -0,0 +1,12 @@
+export default function TodoComp() {
+  return (
+    <>
+      <div className="flex flex-col   ">
+        <div className="ml-1 h-5 mt-5 cursor-pointer ">
+          <span className="material-symbols-outlined text-gray-600  ">add</span>
+        </div>
+        <div className="list"></div>
+      </div>
+    </>
+  );
+}
--- myapp/src/app/todo/page.tsx
+++ myapp/src/app/todo/page.tsx
@@ -1,16 +1,28 @@
 "use client";
 import React from "react";
 import { useState } from "react";
+import TodoComp from "../components/todoComp/todoComp";
 
 export default function TodoPage() {
   const [text, setText] = useState("");
+  const [isShowBtnActive, setShowBtnActive] = useState(false);
+
   return (
-    <div className=" main   flex justify-center mt-40 ">
-      <div className="bg-white min-h-96  w-5/12">
-        <div className="inputBox ml-6 mt-4  ">
+    <div className=" main   flex justify-end mt-40  mr-44  ">
+      <div className="bg-white  min-h-96 flex flex-row   w-2/5 ">
+        {isShowBtnActive ? <TodoComp /> : null}
+        <div
+          className="inputBox flex flex-col ml-2 mt-4 w-full bg-slate-600 h-10 "
+          onClick={() => {
+            setShowBtnActive(true);
+          }}
+          onChange={() => {
+            setShowBtnActive(false);
+          }}
+        >
           <textarea
-            className="textArea  outline-none overflow-hidden w-5/12 "
-            placeholder="Enter your task"
+            className=" outline-none overflow-hidden  "
+            placeholder="Create your todo list"
             onChange={(e) => {
               setText(e.target.value);
             }}
